253a413801d5f91c1a83980d7f1101142f43e9a5
shantokarmoker8 <shantokarmoker8@gmail.com>
2024-03-21 12:20:09 +0600

    propTypes

--- src/component/Link/Link.jsx
+++ src/component/Link/Link.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 const Link = ({ route }) => {
   return (
     <li>
@@ -5,5 +6,7 @@ const Link = ({ route }) => {
     </li>
   );
 };
-
+Link.propTypes = {
+  route: PropTypes.object,
+};
 export default Link;
